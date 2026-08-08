f4dfe7b1233c1102059e70fbb360ab662f934eb4
Anthony Lee <alee792@gmail.com>
2026-08-07 19:07:31 -0700

    Set initial release version

--- internal/version/version_test.go
+++ internal/version/version_test.go
@@ -3,7 +3,7 @@ package version
 import "testing"
 
 func TestValidate(t *testing.T) {
-	for _, value := range []string{"0.9.0-dev", "1.2.3", "1.2.3-alpha.1+build.7"} {
+	for _, value := range []string{"0.1.0-dev", "1.2.3", "1.2.3-alpha.1+build.7"} {
 		if err := Validate(value); err != nil {
 			t.Errorf("Validate(%q): %v", value, err)
 		}
